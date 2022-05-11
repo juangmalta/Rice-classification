46e9d7abdadb14c5536c25b08813de336d4cfc3a
Luis <79459748+luisalcab@users.noreply.github.com>
2022-05-11 18:21:50 -0500

    Plotting more data

--- RiceClassification.py
+++ RiceClassification.py
@@ -1,5 +1,7 @@
 from cProfile import label
 from re import X
+from statistics import linear_regression
+from sklearn import linear_model
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
