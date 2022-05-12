9ce35e1efc04946c4b829acb3cad2c38d30adb19
Luis <79459748+luisalcab@users.noreply.github.com>
2022-05-12 12:13:10 -0500

    Cambios

--- RiceClassification.py
+++ RiceClassification.py
@@ -13,7 +13,7 @@ data = pd.read_csv("./riceClassification.csv", index_col='id')
 data = data.dropna()
 
 #4. Split the data into training and testing sets
-X = data.drop(['Class','Extent'], axis=1)
+X = data.drop('Class', axis=1)
 Y = data['Class']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 clf = LogisticRegression(random_state=0, max_iter=1000).fit(X_train, Y_train)
