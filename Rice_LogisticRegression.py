94f9c934dfdbc5272f48d5c36d0d7303970cd74c
Luis <79459748+luisalcab@users.noreply.github.com>
2022-05-12 23:11:33 -0500

    Minor changes

--- Rice_LogisticRegression.py
+++ Rice_LogisticRegression.py
@@ -14,29 +14,32 @@ data = pd.read_csv("./riceClassification.csv", index_col='id')
 #2. Clean the data
 data = data.dropna()
 
-#3. Split the data into training and testing sets
+#3. Headers of the data
+print(data.columns)
+
+#4. Split the data into training and testing sets
 X = data.drop(['Class','Extent'], axis=1)
 Y = data['Class']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 clf = LogisticRegression(random_state=42, max_iter=1000).fit(X_train, Y_train)
 
-#4. Evaluate the model
+#5. Evaluate the model
 print("Accuracy of Logistic regression classifier on training set: {:.5f}".format(clf.score(X_train, Y_train)))
 print("Accuracy of Logistic regression classifier on test set: {:.5f}".format(clf.score(X_test, Y_test)))
 
-#5. Confusion Matrix
-#5.1 Training set
+#6. Confusion Matrix
+#6.1 Training set
 y_pred = clf.predict(X_train)
 cnf_matrix = metrics.confusion_matrix(Y_train, y_pred)
 print("Confusion matrix train:")
 print(cnf_matrix)
-#5.2 Testing set
+#6.2 Testing set
 y_pred_test = clf.predict(X_test)
 cnf_matrix_test = metrics.confusion_matrix(Y_test, y_pred_test)
 print("Confusion matrix test:")
 print(cnf_matrix_test)
 
-#6. Plot the decision boundary
+#7. Plot the decision boundary
 # Area x Perimeter training set
 ax = plt.axes()
 plt.scatter(X_train['Area'], X_train['Perimeter'], c=Y_train, cmap='autumn')
